4d46202dbb792ffea70f8536459f21f910d3d0bc
Igoris Ivanovas <igoris.x@inbox.lt>
2021-10-02 15:57:05 +0300

    fix

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 const About = () => {
     return (
         <div>
-            <h4>It was me work for study</h4>
+            <h4>It was my work for study</h4>
             <Link to="/">To task list</Link>
         </div>
     )
